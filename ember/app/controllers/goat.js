8db33253197a425b2a6d5794a482507fc16dfcba
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-02-01 15:49:37 -0500

    User can edit goat info

---
+++ ember/app/controllers/goat.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
