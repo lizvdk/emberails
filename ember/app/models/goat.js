f75507cf684fcb8384a2155eefc5aa3bb5e86bf6
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-02-01 14:32:36 -0500

    Add goat model

---
+++ ember/app/models/goat.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    name: DS.attr('string'),
+    bio: DS.attr('string'),
+    snack: DS.attr('string'),
+    isFavorite: DS.attr('boolean')
+});
