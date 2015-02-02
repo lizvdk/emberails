8958bdeb9cbc146f89f17950f35fb7f8ca8c2de6
Liz Vanderkloot <liz.m.vdk@gmail.com>
2015-02-02 12:27:28 -0500

    Delete Fixture data

--- ember/app/models/goat.js
+++ ember/app/models/goat.js
@@ -6,32 +6,3 @@ export default DS.Model.extend({
     snack: DS.attr('string'),
     isFavorite: DS.attr('boolean')
 });
-// .reopenClass({
-//     FIXTURES: [
-//         {
-//           id: 1,
-//           name: "Sally",
-//           bio: "Natoque porta ullamcorper suspendisse eget a accumsan" +
-//                 "at consequat class blandit ultrices odio a suspendisse" +
-//                 "ornare aptent condimentum a aliquet phasellus neque." +
-//                 "Fusce auctor a nisi.",
-//           snack: "Tin Cans",
-//           isFavorite: false
-//         },
-//         {
-//           id: 2,
-//           name: "Frostie",
-//           bio: "Baby goat caught in an endless, boundlessly happy frolic.",
-//           snack: "Hay",
-//           isFavorite: true
-//         },
-//         {
-//           id: 3,
-//           name: "T.",
-//           bio: "Netus ridiculus arcu ad parturient libero dui sem inceptos" +
-//                "id quis a scelerisque a dignissim parturient sem.",
-//           snack: "Chai Tea",
-//           isFavorite: false
-//         }
-//     ]
-// });
